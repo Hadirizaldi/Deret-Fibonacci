cb33cb04d2b924a22a3220c7bd65604db7dc3cc0
Hadirizaldi <Hadirizaldi.hr@gmail.com>
2021-03-15 13:51:50 +0700

    Edit File Main.dart

--- bin/Main.dart
+++ bin/Main.dart
@@ -4,6 +4,8 @@ import 'Choice.dart';
 void main(List<String> argument){
   stdout.write("Menampilkan deret Fibonacci yang diminta \n");
   stdout.write("======================================== \n");
+  stdout.write("1\t untuk menampilkan semua deret fibonacci\n");
+  stdout.write("2\t untuk menampilkan nilai deret fibonacci\n");
   stdout.write("pilihan = ");
   int b = int.tryParse(stdin.readLineSync());
 
